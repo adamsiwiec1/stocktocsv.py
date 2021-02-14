25b6b99dcfd4c270c26ea57a1fe1758529e69be8
Adam Siwiec <acs0029@mail.wvu.edu>
2021-02-14 00:41:20 -0500

    update w/ exe

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 import py2exe
-import sys
+import sys, os
 
 # sys.argv.append('stock2csv')
 
-setup(#name='stock2csv',
-      # version='1.0.0',
-      # description='Takes in a list of stock tickers along with a start date, end date, and file path from the user then'
-      #             'creates .csv files for each stock containing data between the specified times.',
-      # author='Adam Siwiec',
-      # author_email='adam2.siwiec@gmail.com',
-      # url='https://github.com/adamsiwiec1/stocktocsv',
-      # # package_dir={'Lib': 'Z:\\test'},
-      # # options={'py2exe': {'includes': ['lxml._elementpath']}},
-      console=['stock2csv.py']
+setup(name='stock2csv',
+       version='1.0.0',
+       description='Takes in a list of stock tickers along with a start date, end date, and file path from the user then'
+                   'creates .csv files for each stock containing data between the specified times.',
+       author='Adam Siwiec',
+       author_email='adam2.siwiec@gmail.com',
+       url='https://github.com/adamsiwiec1/stock2csv',
+       # options={'py2exe': {'bundle_files': 2}},
+      windows=[{'script': 'stock2csv.py'}],
+      zipfile=None
       )
